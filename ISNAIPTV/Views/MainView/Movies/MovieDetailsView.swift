46b9397d2525bf5df8f3f0168ba768757a0e379b
Issam Souabni <souabni.issam@gmail.com>
2023-05-14 11:14:43 +0200

    modify movieDetailView layout

--- ISNAIPTV/Views/MainView/Movies/MovieDetailsView.swift
+++ ISNAIPTV/Views/MainView/Movies/MovieDetailsView.swift
@@ -16,135 +16,130 @@ struct MovieDetailsView: View {
     @State var displayVideo : Bool = false
     
     var body: some View {
-        VStack(alignment: .leading){
-            ZStack(alignment: .bottom){
-                ZStack(alignment: .topTrailing){
-                    ZStack{
-                        if let movieImage = selectedMovie.iconUrl, let movieImageURL = URL(string:movieImage){
-                            KFImage(movieImageURL)
-                                .placeholder {
-                                    Image("placeholderMovie")
-                                        .resizable()
-                                        .scaledToFill()
-                                }
-                                .resizable()
-                                .scaledToFill()
-                                .frame(maxWidth:.infinity)
-                                .frame(height: 300,alignment: .top)
-                                .clipped()
-                        }
-                        Button{
-                            displayVideo = true
-                            let value = UIInterfaceOrientation.landscapeRight.rawValue
-                            UIDevice.current.setValue(value, forKey: "orientation")
-                        } label:{
-                            Image("play")
-                                .resizable()
-                                .frame(width: 50, height: 50)
-                                .foregroundColor(Color.white)
-                                .padding(4)
-                                .background(Circle()
-                                    .strokeBorder(Color.clear,lineWidth: 0)
-                                    .background(.ultraThinMaterial)
-                                    .cornerRadius(.infinity))
-                        }
-                    }
-                    Button{
-                        presentationMode.wrappedValue.dismiss()
-                    } label:{
-                        Image("close")
+        GeometryReader{geometry in
+            VStack{
+                NavigationBar(title: "") {
+                    presentationMode.wrappedValue.dismiss()
+                }
+                Spacer()
+                
+                
+                HStack(alignment: .center){
+                    
+                    if let movieImage = selectedMovie.iconUrl, let movieImageURL = URL(string:movieImage){
+                        KFImage(movieImageURL)
+                            .placeholder {
+                                Image("placeholderMovie")
+                                    .resizable()
+                                    .scaledToFill()
+                            }
                             .resizable()
-                            .frame(width: 15, height: 15)
-                            .frame(width: 30, height: 30)
-                            .background(Circle()
-                                .strokeBorder(Color.clear,lineWidth: 0)
-                                .background(.ultraThinMaterial)
-                                .cornerRadius(.infinity))
-                            .padding(.trailing,20)
-                            .padding(.top,50)
+                            .scaledToFit()
+                            .frame(maxWidth:.infinity)
+                            .frame(width:400,height: 300)
+                        
                     }
-                }
-                LinearGradient(gradient: Gradient(colors: [.clear, .black]), startPoint: .top, endPoint: .bottom)
-                    .frame(height: 100)
-            }
-            VStack(alignment:.leading,spacing:12){
-                HStack{
-                    Text(selectedMovie.name )
-                        .foregroundColor(Color.white)
-                        .font(.custom(RobotoFont.bold.rawValue, size: 16))
                     
-                    Text(selectedMovie.info?.info?.age ?? "")
-                    Spacer()
-//                    Button{
-////                        let documentDirectory = FileManager.default.urls(for: .documentDirectory,
-////                                                                        in: .userDomainMask)[0]
-////                        let movieName = documentDirectory.appendingPathComponent("movies\(selectedMovie.movieID)")
-////
-////                        if let movieUrl = URL(string: selectedMovie.url) {
-////                            // 3
-////                            URLSession.shared.downloadTask(with: movieUrl) { (tempFileUrl, response, error) in
-////
-////                                // 4
-////                                if let imageTempFileUrl = tempFileUrl {
-////                                    do {
-////                                        // Write to file
-////                                        // 1
-////                                        let movieData = try Data(contentsOf: imageTempFileUrl)
-////
-////                                        // 2
-////                                        try movieData.write(to: movieName)
-////                                    } catch {
-////                                        print("Error")
-////                                    }
-////                                }
-////                            }.resume()
-////                        }
-//                    } label:{
-//                    Image("download")
-//                        .resizable()
-//                        .scaledToFit()
-//                        .frame(width: 20, height: 20)
-//                        .padding(.trailing,8)
-//                    }
-                }
-                .frame(height:48)
-                
-                HStack{
-                    ForEach( selectedMovie.genres,id:\.self){genre in
-                        Text(genre)
-                            .font(.custom(RobotoFont.bold.rawValue, size: 14))
-                            .padding(5)
-                            .background(.ultraThinMaterial)
-                            .cornerRadius(4)
+                    VStack(alignment:.leading,spacing:24){
+                        HStack{
+                            Text(selectedMovie.name )
+                                .foregroundColor(Color.white)
+                                .font(.custom(RobotoFont.bold.rawValue, size: 16))
+                            
+                            Text(selectedMovie.info?.info?.age ?? "")
+                            Spacer()
+                            //                    Button{
+                            ////                        let documentDirectory = FileManager.default.urls(for: .documentDirectory,
+                            ////                                                                        in: .userDomainMask)[0]
+                            ////                        let movieName = documentDirectory.appendingPathComponent("movies\(selectedMovie.movieID)")
+                            ////
+                            ////                        if let movieUrl = URL(string: selectedMovie.url) {
+                            ////                            // 3
+                            ////                            URLSession.shared.downloadTask(with: movieUrl) { (tempFileUrl, response, error) in
+                            ////
+                            ////                                // 4
+                            ////                                if let imageTempFileUrl = tempFileUrl {
+                            ////                                    do {
+                            ////                                        // Write to file
+                            ////                                        // 1
+                            ////                                        let movieData = try Data(contentsOf: imageTempFileUrl)
+                            ////
+                            ////                                        // 2
+                            ////                                        try movieData.write(to: movieName)
+                            ////                                    } catch {
+                            ////                                        print("Error")
+                            ////                                    }
+                            ////                                }
+                            ////                            }.resume()
+                            ////                        }
+                            //                    } label:{
+                            //                    Image("download")
+                            //                        .resizable()
+                            //                        .scaledToFit()
+                            //                        .frame(width: 20, height: 20)
+                            //                        .padding(.trailing,8)
+                            //                    }
+                        }
+                        .frame(height:48)
+                        
+                        HStack{
+                            ForEach( selectedMovie.genres,id:\.self){genre in
+                                Text(genre)
+                                    .font(.custom(RobotoFont.bold.rawValue, size: 14))
+                                    .padding(5)
+                                    .background(.ultraThinMaterial)
+                                    .cornerRadius(4)
+                            }
+                        }
+                        
+                        HStack{
+                            Text(selectedMovie.info?.info?.releaseDate ?? "")
+                            
+                            
+                            if let durationSecs = selectedMovie.info?.info?.durationSecs as? Int?, let duration = durationSecs{
+                                Text("| \(duration/60)'")
+                            }
+                            if let durationSecs = selectedMovie.info?.info?.durationSecs as? String{
+                                Text("| \(durationSecs)")
+                            }
+                        }
+                        .foregroundColor(Color.gray)
+                        .font(.custom(RobotoFont.regular.rawValue, size: 14))
+                        
+                        Text(selectedMovie.info?.info?.description ?? "")
+                        Text("Cast : \(selectedMovie.info?.info?.cast ?? "")")
+                            .font(.custom(RobotoFont.regular.rawValue, size: 14))
+                            .foregroundColor(Color.gray)
+                        
+                        //Spacer()
+                        
+                        HStack{
+                            Spacer()
+                            Button{
+                                displayVideo = true
+                                let value = UIInterfaceOrientation.landscapeRight.rawValue
+                                UIDevice.current.setValue(value, forKey: "orientation")
+                            } label:{
+                                Text("Play")
+                                    .padding(EdgeInsets(top: 5, leading: 30, bottom: 5, trailing: 30))
+                                    .background(Color.lightBlue)
+                                    .foregroundColor(Color.black)
+                                
+                            }
+                            Spacer()
+                        }
+                        
                     }
-                }
-                
-                HStack{
-                    Text(selectedMovie.info?.info?.releaseDate ?? "")
-                       
+                    .padding(.horizontal, 8)
+                    .font(.custom(RobotoFont.regular.rawValue, size: 16))
+                    .frame(width:geometry.size.width/2)
                     
-                    if let durationSecs = selectedMovie.info?.info?.durationSecs as? Int?, let duration = durationSecs{
-                        Text("| \(duration/60)'")
-                    }
-                    if let durationSecs = selectedMovie.info?.info?.durationSecs as? String{
-                        Text("| \(durationSecs)")
-                    }
                 }
-                .foregroundColor(Color.gray)
-                .font(.custom(RobotoFont.regular.rawValue, size: 14))
-                
-                Text(selectedMovie.info?.info?.description ?? "")
-                Text("Cast : \(selectedMovie.info?.info?.cast ?? "")")
-                    .font(.custom(RobotoFont.regular.rawValue, size: 14))
-                    .foregroundColor(Color.gray)
-                
+                .foregroundColor(Color.white)
                 Spacer()
             }
-            .padding(.horizontal, 8)
-            .font(.custom(RobotoFont.regular.rawValue, size: 16))
         }
         .background(Color.black)
-        .foregroundColor(Color.white)
         .ignoresSafeArea()
         .onAppear{
             currentXtreamSession.xtreamAgent.getVodInfo(vodID: selectedMovie.movieID) { vodInfo in
